71a3dd82826967a1a2272825a096e980ba8c8ee6
Emi Tanaka <dr.emi.tanaka@gmail.com>
2020-07-09 10:39:05 +1000

    helpers

---
+++ R/helpers.R
@@ -0,0 +1,11 @@
+ofal_control <- function(ncores = NULL,
+                         eps = 1e-3,
+                         max_iter = 100,
+                         max_tuning = 0.5,
+                         trace = FALSE,
+                         seed = NULL,
+                         ...) {
+  use_multicore(ncores)
+
+  list(eps = eps, max_iter = max_iter, trace = trace, max_tuning = max_tuning, seed = seed)
+}
